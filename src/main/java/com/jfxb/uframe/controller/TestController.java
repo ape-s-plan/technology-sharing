6f9ef51ff0dc1e37ed0154c19e090ef8e8b38e27
shenye5377 <306763484@qq.com>
2021-03-24 10:48:53 +0800

    接口路径修正

--- src/main/java/com/jfxb/uframe/controller/TestController.java
+++ src/main/java/com/jfxb/uframe/controller/TestController.java
@@ -5,7 +5,6 @@ import com.jfxb.uframe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,15 +15,11 @@ public class TestController {
 
     @PostMapping("/addUser")
     public ResponseEntity<?> addUser(@RequestBody User user) throws Exception {
-        try {
-            userService.addUser(user);
-            return new ResponseEntity(HttpStatus.OK);
-        }catch (Exception e){
-            throw e;
-        }
+        userService.addUser(user);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
-    @GetMapping("/getAll")
+    @GetMapping("/api/getAll")
     public ResponseEntity<?> getAll() throws Exception {
         return new ResponseEntity(userService.getAll(), HttpStatus.OK);
     }
